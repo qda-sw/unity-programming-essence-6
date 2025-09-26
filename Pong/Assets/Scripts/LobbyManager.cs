ef4cd34d2f814d42061ff2db4bd20bb6915de0e5
SeongWooJeong <jsw0510@pusan.ac.kr>
2025-09-27 01:15:07 +0900

    feat(#17): 로비 구현
    - closes #17

--- Pong/Assets/Scripts/LobbyManager.cs
+++ Pong/Assets/Scripts/LobbyManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Unity.Netcode;
 using UnityEngine.SceneManagement;
@@ -19,30 +20,55 @@ public class LobbyManager : NetworkBehaviour
     // OnNetworkSpawn은 NetworkBehaviour가 생성될 때 호출됨
     public override void OnNetworkSpawn()
     {
+        _clientReadyStates.Add(NetworkManager.Singleton.LocalClientId, false);
+        if (IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+            NetworkManager.Singleton.SceneManager.OnLoadComplete += OnClientSceneLoadComplete;
+        }
 
+        UpdateLobbyText();
     }
 
     private void OnDisable()
     {
-
+        if (IsServer && NetworkManager.Singleton != null)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnected;
+            NetworkManager.Singleton.SceneManager.OnLoadComplete -= OnClientSceneLoadComplete;
+        }
     }
 
     // 클라이언트가 연결되었을때 실행할 콜백
     private void OnClientConnected(ulong clientId)
     {
-
+        _clientReadyStates.Add(clientId, false);
+        UpdateLobbyText();
     }
 
     // 클라이언트가 씬 로드를 완료했을 때 실행할 콜백
     private void OnClientSceneLoadComplete(ulong clientId, string sceneName, LoadSceneMode loadSceneMode)
     {
+        if (sceneName != "Lobby") return;
 
+        foreach (var pair in _clientReadyStates)
+        {
+            SetClientIsReadyClientRpc(pair.Key, pair.Value);
+        }
     }
 
     // 클라이언트 접속이 끊겼을 때 실행할 콜백
     private void OnClientDisconnected(ulong clientId)
     {
+        if (_clientReadyStates.ContainsKey(clientId))
+        {
+            _clientReadyStates.Remove(clientId);
+        }
 
+        RemovePlayerClientRpc(clientId);
+        UpdateLobbyText();
     }
 
     // 서버가 다른 클라이언트들에게 어떤 클라이언트의 준비 상태가 변경됨을 알리는 RPC 메서드
@@ -50,7 +76,10 @@ public class LobbyManager : NetworkBehaviour
     [ClientRpc]
     private void SetClientIsReadyClientRpc(ulong clientId, bool isReady)
     {
+        if (IsServer) return;
 
+        _clientReadyStates[clientId] = isReady;
+        UpdateLobbyText();
     }
 
     // 서버가 다른 클라이언트들에게 어떤 클라이언트의 준비 상태가 변경됨을 알리는 RPC 메서드
@@ -58,31 +87,75 @@ public class LobbyManager : NetworkBehaviour
     [ClientRpc]
     private void RemovePlayerClientRpc(ulong clientId)
     {
+        if (IsServer) return;
 
+        if (_clientReadyStates.ContainsKey(clientId))
+        {
+            _clientReadyStates.Remove(clientId);
+        }
+        UpdateLobbyText();
     }
 
     // 로비 텍스트를 갱신
     private void UpdateLobbyText()
     {
+        var sb = new StringBuilder();
+        foreach (var pair in _clientReadyStates)
+        {
+            sb.AppendLine($"Player {pair.Key}: {(pair.Value ? "Ready" : "Not Ready")}");
+        }
 
+        lobbyText.text = sb.ToString();
     }
 
     // 게임을 시작할 수 있는지 확인
     private bool CheckIsReadyToStart()
     {
+        if (_clientReadyStates.Count < MinimumReadyCountToStartGame)
+        {
+            return false;
+        }
+
+        foreach (var value in _clientReadyStates.Values)
+        {
+            if (!value) return false;
+        }
+
         return true;
     }
 
     // 게임 시작
     private void StartGame()
     {
+        if (!IsServer) return;
 
+        NetworkManager.OnClientConnectedCallback -= OnClientConnected;
+        NetworkManager.OnClientDisconnectCallback -= OnClientDisconnected;
+        NetworkManager.SceneManager.OnLoadComplete -= OnClientSceneLoadComplete;
+
+        NetworkManager.Singleton.SceneManager.LoadScene("InGame", LoadSceneMode.Single);
     }
 
     // 클라이언트가 준비 버튼을 눌렀을때 실행하는 메서드
     public void SetPlayerIsReady()
     {
+        var localClientId = NetworkManager.Singleton.LocalClientId;
+        var isReady = _clientReadyStates[localClientId] = !_clientReadyStates[localClientId];
 
+        UpdateLobbyText();
+
+        if (IsServer)
+        {
+            SetClientIsReadyClientRpc(localClientId, isReady);
+            if (CheckIsReadyToStart())
+            {
+                StartGame();
+            }
+        }
+        else
+        {
+            SetClientIsReadyServerRpc(localClientId, isReady);
+        }
     }
 
     // 클라이언트가 준비 상태가 변경됬음을 서버에게 알리기 위한 RPC 메서드
@@ -90,6 +163,13 @@ public class LobbyManager : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     private void SetClientIsReadyServerRpc(ulong clientId, bool isReady)
     {
+        _clientReadyStates[clientId] = isReady;
+        SetClientIsReadyClientRpc(clientId, isReady);
+        UpdateLobbyText();
 
+        if (CheckIsReadyToStart())
+        {
+            StartGame();
+        }
     }
 }
